e7d5027b3b3d39395cbb6645ef5c0e5bff30cd5b
rynoterblanche <ryno.terblanche@gmail.com>
2017-05-04 19:46:59 +0100

    Added offers feature

--- src/main/java/com/rt/shop/Shop.java
+++ src/main/java/com/rt/shop/Shop.java
@@ -1,22 +1,75 @@
 package com.rt.shop;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.common.collect.ImmutableMap;
 
 public class Shop {
-	
+
 	private ImmutableMap<String, BigDecimal> prices;
+	private ImmutableMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> offers = null;
+	
+	private BiFunction<String, BigDecimal, BigDecimal> getPrice = (item, qty) -> prices.getOrDefault(item, BigDecimal.ZERO).multiply(qty);
+	private BiFunction<String, BigDecimal, BigDecimal> getDiscount = (item, qty)  -> offers.getOrDefault(item, (a, b) -> BigDecimal.ZERO)
+			.apply(prices.get(item), qty);
 
 	public Shop(ImmutableMap<String, BigDecimal> prices) {
 		this.prices = prices;
 	}
-	
-	public BigDecimal sell(List<String> items) {
-		return items.stream()
-				.map(item -> prices.getOrDefault(item, BigDecimal.ZERO))
-				.reduce(BigDecimal.ZERO, BigDecimal::add);
+
+	public Shop withOffers(ImmutableMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> offers) {
+		this.offers = offers;
+		return this;
 	}
 
+	public BigDecimal sell(List<String> items) {
+		return getFinalTotalPricePerItem(items).entrySet().stream()
+                .map(e -> e.getValue())
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .setScale(2, RoundingMode.HALF_EVEN);
+	}
+
+	public Map<String, BigDecimal> getItemsGroupedAndTotalled(List<String> items) {
+		return items.stream().collect(
+				Collectors.groupingBy(item -> item, 
+						Collectors.reducing(BigDecimal.ZERO, a -> BigDecimal.ONE, BigDecimal::add)));
+	}
+	
+	public Map<String, BigDecimal> getTotalPricePerItem(List<String> items) {
+		return getItemsGroupedAndTotalled(items).entrySet().stream().collect(
+				Collectors.toMap(
+						e -> e.getKey(), 
+						e -> getPrice.apply(e.getKey(), e.getValue())));
+	}
+	
+	public Map<String, BigDecimal> getTotalDiscountPerItem(List<String> items) {
+		if (offers == null)
+			return Collections.emptyMap();
+		
+		return getItemsGroupedAndTotalled(items).entrySet().stream().collect(
+				Collectors.toMap(
+						e -> e.getKey(), 
+						e -> getDiscount.apply(e.getKey(), e.getValue())));
+	}
+		
+	public Map<String, BigDecimal> getFinalTotalPricePerItem(List<String> items) {
+		return Stream.of(getTotalPricePerItem(items), getTotalDiscountPerItem(items))
+			.map(Map::entrySet)
+			.flatMap(Collection::stream)
+			.collect(
+					Collectors.toMap(
+							Map.Entry::getKey,
+							Map.Entry::getValue,
+							BigDecimal::subtract)
+						);
+	}
+	
 }
--- src/test/java/com/rt/shop/test/TestShop.java
+++ src/test/java/com/rt/shop/test/TestShop.java
@@ -11,35 +11,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.function.BiFunction;
 
 public class TestShop {
 	
+	static final String ORANGE = "Orange";
+	static final String APPLE = "Apple";
+	
 	Shop shop;
-	ImmutableMap<String, BigDecimal> prices;
-
+	ImmutableMap<String, BigDecimal> shopItems;
+	ImmutableMap<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>> shopOffers;
+	
+	// Here we can supply any type of discount calculations we require
+	static final BiFunction<BigDecimal, BigDecimal, BigDecimal> TWO_FOR_ONE = (price, qty) -> price.multiply(qty.divideToIntegralValue(new BigDecimal(2)));
+	static final BiFunction<BigDecimal, BigDecimal, BigDecimal> THREE_FOR_TWO = (price, qty) -> price.multiply(qty.divideToIntegralValue(new BigDecimal(3)));
+	
 	@Before
 	public void init() {
-		prices = new ImmutableMap.Builder<String, BigDecimal>()
-				.put("Apple", BigDecimal.valueOf(.60))
-				.put("Orange", BigDecimal.valueOf(.25))
+		shopItems = new ImmutableMap.Builder<String, BigDecimal>()
+				.put(APPLE, BigDecimal.valueOf(.60))
+				.put(ORANGE, BigDecimal.valueOf(.25))
 				.build();
-		shop = new Shop(prices);
+		shopOffers = new ImmutableMap.Builder<String, BiFunction<BigDecimal, BigDecimal, BigDecimal>>()
+				.put(APPLE, TWO_FOR_ONE)
+				.put(ORANGE, THREE_FOR_TWO)
+				.build();
+		shop = new Shop(shopItems);
 	}
 
 	@Test
 	public void shouldSellItemsAtCorrectTotalCost() {
-		assertThat(shop.sell(Arrays.asList("Apple", "Orange"))).isEqualTo(BigDecimal.valueOf(.85));
-		assertThat(shop.sell(Arrays.asList("Apple", "Apple", "Orange", "Apple"))).isEqualTo(BigDecimal.valueOf(2.05));
+		assertThat(shop.sell(Arrays.asList(APPLE, ORANGE))).isEqualTo(BigDecimal.valueOf(.85).setScale(2));
+		assertThat(shop.sell(Arrays.asList(APPLE, APPLE, ORANGE, APPLE))).isEqualTo(BigDecimal.valueOf(2.05).setScale(2));
 	}
 	
 	@Test
 	public void shouldReturnZeroCostForEmptyItemList() {
-		assertThat(shop.sell(Collections.emptyList())).isEqualTo(BigDecimal.ZERO);
+		assertThat(shop.sell(Collections.emptyList())).isEqualTo(BigDecimal.ZERO.setScale(2));
 	}
 	
 	@Test
 	public void shouldIgnoreUnknownItemsAndNotFail() {
-		assertThat(shop.sell(Arrays.asList("Apple", "SomeItem"))).isEqualTo(BigDecimal.valueOf(.60));
+		assertThat(shop.sell(Arrays.asList(APPLE, "SomeItem"))).isEqualTo(BigDecimal.valueOf(.60).setScale(2));
 	}
 	
+	@Test
+	public void shouldSellItemsOnOfferAtCorrectTotalCost() {
+		assertThat(shop.withOffers(shopOffers).sell(Arrays.asList(APPLE, APPLE))).isEqualTo(BigDecimal.valueOf(.60).setScale(2));
+		assertThat(shop.withOffers(shopOffers).sell(Arrays.asList(APPLE, APPLE, APPLE))).isEqualTo(BigDecimal.valueOf(1.20).setScale(2));
+		assertThat(shop.withOffers(shopOffers).sell(Arrays.asList(APPLE, APPLE, APPLE, APPLE))).isEqualTo(BigDecimal.valueOf(1.20).setScale(2));
+		assertThat(shop.withOffers(shopOffers).sell(Arrays.asList(ORANGE, ORANGE, ORANGE))).isEqualTo(BigDecimal.valueOf(.50).setScale(2));
+		assertThat(shop.withOffers(shopOffers).sell(Arrays.asList(ORANGE, ORANGE, ORANGE, APPLE, APPLE))).isEqualTo(BigDecimal.valueOf(1.10).setScale(2));
+	}
 }
